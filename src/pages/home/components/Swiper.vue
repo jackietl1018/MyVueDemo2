3e1160d727ca1cb6a5126a0bf4f31d859c70fd3b
jackietl1018 <1491704844@qq.com>
2019-10-08 10:29:40 +0800

    add carousel-function

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption" >
+            <swiper-slide v-for="item of swiperList" :key="item.id">
+                <img class="swiper-img" :src="item.imgUrl" alt="">
+            </swiper-slide>
+            <div class="swiper-pagination"  slot="pagination"></div>
+        </swiper>
+    </div> 
+</template>
+<script>
+export default {
+    name:'HomeSwiper',
+    data () {
+        return {
+            swiperOption:{
+                pagination: '.swiper-pagination',
+                paginationClickable:true,      //这行使得点击轮播图里小圆点时，能够滑动
+                speed:1000,                    //点击小圆点滑动的速度为1000毫秒（1秒）
+                autoplayDisableOnInteraction : false,
+                autoplay:1500,
+                loop: true
+            },
+            swiperList: [
+                {
+                    id: '0001',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1810/e0/c99056cd01148902.jpg_750x200_8b93d0b5.jpg'
+                },
+                {
+                    id: '0002',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1808/7e/19b06067afac3402.jpg_750x200_f613313a.jpg'
+                },
+                {
+                    id: '0003',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/2d/0d7dc3400a50b502.jpg_750x200_4ca95dd4.jpg'
+                }
+            ]
+        }
+    }
+}
+</script>
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background #ffffff
+    .wrapper
+        width 100%
+        height 0
+        overflow hidden
+        padding-bottom 26.66%
+        background #eeeeee
+        // height 31.25vw (浏览器有差异，不建议)
+        .swiper-img
+            width 100% 
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.use(Router)
+Vue.use(VueAwesomeSwiper)
 
 export default new Router({
   routes: [
