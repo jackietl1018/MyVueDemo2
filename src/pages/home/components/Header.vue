61aad1558348cd46f40ed069d6ba5178eb506805
jackietl1018 <1491704844@qq.com>
2019-10-08 09:28:50 +0800

    commit

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="header-input">
       <i class="iconfont">&#xe616;</i>
-      输入城市/经典/游玩主题
+      <span>输入城市/景点/游玩主题</span>
     </div>
     <router-link></router-link>
       <div class="header-right">城市</div>
@@ -27,23 +27,32 @@
 <style lang="stylus" scoped>
   .header 
     display: flex 
-    line-height: .86rem 
+    height: 44px
     background: #00bcd4 
     color: #fff 
   .header-left 
-    width: .64rem 
+    width: 44px
+    height: 44px
+    line-height: 44px
     float: left 
   .header-input 
     flex: 1 
-    height: .64rem 
-    line-height .64rem 
-    margin-left: .2rem 
-    margin-top: .12rem 
+    height: 30px
+    line-height 30px 
+    margin-left: auto  
+    margin-top: 7px 
     background: #ffffff 
-    border-radius: .1rem 
+    border-radius: 1px 
     color: #ccc 
+  .iconfont
+    float: left
+    margin-left: 8px
+  .iconfont span 
+    float: left
   .header-right 
-    width: 1.24rem 
+    width: 66px
+    height: 44px
+    line-height: 44px
     float: right 
     text-align : center
 </style>
